d8ea28568e2d214efbc106ef651fa9d8a9adef23
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2022-10-15 01:56:45 +0530

    Time: 0 ms (100.00%), Space: 6 MB (32.18%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.cpp
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -12,7 +12,7 @@ public:
         
         int ans = 1;
         
-        for(int i=2;i<=gcd(a,b);i++)
+        for(int i=2;i<=a;i++)
         {
             if(a%i == 0 && b%i == 0)
                 ans++;
