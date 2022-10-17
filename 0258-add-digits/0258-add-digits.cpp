21dbacb8ce4aff8ae77eb04f6bd63bd958264b26
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2022-10-17 18:58:30 +0530

    Time: 7 ms (22.40%), Space: 6 MB (25.43%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,22 @@
+class Solution 
+{
+public:
+    int addDigits(int num) 
+    {        
+        if(num/10 == 0)
+        {            
+            return num;
+        }
+        else
+        {
+            int sum = 0;
+            while(num!=0)
+            {
+                sum=sum + (num%10);
+                num=num/10;
+            }
+            num = addDigits(sum);
+        }
+        return num;
+    }
+};
