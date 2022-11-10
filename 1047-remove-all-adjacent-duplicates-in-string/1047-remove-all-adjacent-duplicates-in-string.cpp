9905873c14c1afb7cc6ca390bdc51bc1fc981369
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2022-11-10 14:38:20 +0530

    Time: 11 ms (99.30%), Space: 10.8 MB (74.37%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,27 @@
+class Solution 
+{
+public:
+    string removeDuplicates(string s) 
+    {
+        string ans={};
+        for(int i=0;i<=s.size()-1;i++)
+        {
+            if(ans.size()==0)
+            {
+                ans.push_back(s[i]);                
+            }
+            else
+            {
+                if(s[i]==ans.back())
+                {
+                    ans.pop_back();                    
+                }
+                else
+                {
+                    ans.push_back(s[i]);
+                }
+            }            
+        }
+        return ans;
+    }
+};
