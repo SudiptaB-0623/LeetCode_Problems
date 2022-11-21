a352dac862914bfe86031de6d67e1263ec5747fa
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2022-11-21 16:04:32 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (21.09%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -0,0 +1,21 @@
+class Solution 
+{
+public:
+    bool isThree(int n) 
+    {
+        int count = 1;
+        
+        for(int i=2;i<=n;i++)
+        {
+            if(n%i==0)
+                count++;
+            if(count>3)
+                return false;
+            
+        }
+        if(count==3)
+                return true;
+        
+        return false;
+    }
+};
