c4dfbaad454a3c632d6b9e4aae6c11b9699705a5
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2022-12-24 23:16:37 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (54.83%) - LeetHub

---
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
@@ -0,0 +1,19 @@
+class Solution 
+{
+public:
+    vector<int> sumZero(int n) 
+    {
+        vector<int> ans;
+        
+        for(int i=1;i<=(n/2);i++)
+        {
+            ans.push_back(i);
+            ans.push_back(i*(-1));
+        }
+        
+        if(n%2!=0)
+            ans.push_back(0);
+        
+        return ans;
+    }
+};
