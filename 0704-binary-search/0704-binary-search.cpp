37b4dfe91a372c0a43d889dfb4d1aa8806131ed2
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-04-02 01:21:26 +0530

    Time: 45 ms (27.11%), Space: 27.6 MB (69.63%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,27 @@
+class Solution 
+{
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        int low = 0, high = nums.size()-1;
+        int ans = -1;
+        
+        while(low<=high)
+        {
+            int mid = low+(high-low)/2;
+            cout<<mid;
+            
+            if(target>nums[mid])
+                low = mid + 1;
+            else if(target<nums[mid])
+                high = mid - 1;
+            else
+            {
+                ans = mid;
+                break;
+            }
+        }
+        
+        return ans;
+    }
+};
