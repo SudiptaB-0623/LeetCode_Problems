8e69ec029b8a4463bd8314c92c7ce729e8398479
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-05-09 01:03:56 +0530

    Time: 11 ms (87.18%), Space: 11.2 MB (92.07%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -1,19 +1,20 @@
-class Solution 
-{
+class Solution {
 public:
-    int diagonalSum(vector<vector<int>>& mat) 
-    {
-        int sum=0;
-        for(int i=0,j=mat.size()-1;i<mat.size();i++,j--)
-        {
-            sum= sum + mat[i][i] + mat[i][j];
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            // Add elements from primary diagonal.
+            ans += mat[i][i];
+            // Addd elements from secondary diagonal.
+            ans += mat[n - 1 - i][i];
         }
-        
-        if(mat.size()%2 == 1)
-        {
-            sum = sum - mat[mat.size()/2][mat.size()/2];
+        // If n is odd, subtract the middle element as its added twice.
+        if (n % 2 != 0) {
+            ans -= mat[n / 2][n / 2];
         }
-                                             
-        return sum;
+
+        return ans;
     }
 };
