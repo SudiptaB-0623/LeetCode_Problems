294addf6ee0da6e05c518ad05f79b9102dfdc9af
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-06-03 23:26:03 +0530

    Time: 326 ms (55.97%), Space: 112.8 MB (79.84%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,28 @@
+class Solution 
+{
+public:
+    int maxTime = INT_MIN;
+    
+    void DFS(vector<int> adjList[], vector<int>& informTime, int curr, int time) 
+    {
+        
+        maxTime = max(maxTime, time);        
+        for (int adjacent : adjList[curr]) 
+        {             
+            DFS(adjList, informTime, adjacent, time + informTime[curr]);
+        }
+    }
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) 
+    {
+        vector<int> adjList[n];        
+        
+        for (int i = 0; i < n; i++) 
+        {
+            if (manager[i] != -1) {
+                adjList[manager[i]].push_back(i);
+            }
+        }        
+        DFS(adjList, informTime, headID, 0);
+        return maxTime;
+    }
+};
