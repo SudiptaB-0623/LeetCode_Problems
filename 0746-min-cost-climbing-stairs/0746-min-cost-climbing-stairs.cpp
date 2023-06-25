2c1139b54e77e79f84298014e9908cbb6da7480e
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-06-25 19:24:32 +0530

    Time: 27 ms (5.02%), Space: 13.8 MB (18.36%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,39 +1,21 @@
 class Solution 
 {
 public:
-    int jump(int ind, int ans, vector<int> &dp, vector<int> cost)
-    {
-        if(ind == 0)
-        {
-            ans = cost[0];
-            dp[ind] = cost[0];
-            return ans;
-        }
-        if(ind == 1)
-        {
-            ans = min(cost[1], cost[1]+cost[0]);
-            dp[ind] = ans;
-            return ans;
-        }
-        
-        if(ind!=cost.size() && dp[ind]!= -1)
-            return dp[ind];
-        
-        int left = jump(ind-1, ans, dp, cost);
-        int right = jump(ind-2, ans, dp, cost);
-        
-        if(ind == cost.size())
-            return min(left, right);
-        else        
-            return dp[ind] = min(left, right)+cost[ind];
-    }
+   
     int minCostClimbingStairs(vector<int>& cost) 
     {
         vector<int> dp(cost.size(), -1);
         int ans = 0;
+        dp[0] = cost[0];
+        dp[1] = min(cost[1], cost[1]+cost[0]);
+        cout<<dp[0]<<" "<<dp[1]<<" ";
         
-        ans = jump(cost.size(), ans, dp, cost);
-        
-        return ans;
+        for(int i=2; i<cost.size();i++)
+        {
+            dp[i] =  min(dp[i-1], dp[i-2]) + cost[i];
+            cout<<dp[i]<<" ";
+        }
+                        
+        return min(dp[cost.size()-1], dp[cost.size()-2]);
     }
 };
