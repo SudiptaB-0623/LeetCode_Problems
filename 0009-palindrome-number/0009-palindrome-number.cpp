937d61c33e710dfdff26ea3197424b53fe44e7ee
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-06-26 22:35:23 +0530

    Time: 12 ms (76.18%), Space: 5.8 MB (99.62%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,30 +3,23 @@ class Solution
 public:
     bool isPalindrome(int x) 
     {
-        if(x<0 && (x/10 == 0))
+        if(x<0 || x==INT_MAX || x==INT_MIN)
             return false;
-        if(x==0)
+        
+        int temp = x;
+        int rev = 0;
+        
+        while(temp!=0)
+        {
+            if(rev>INT_MAX/10)
+                return 0;
+            
+            rev = rev*10 + temp%10;
+            temp = temp/10;
+        }
+        if(rev == x)
             return true;
-        vector<int> vec;
-        
-        int y = x;
-        
-        while(y!=0)
-        {
-            if(y/10 == 0)
-                vec.push_back(y%10);
-            else                  
-                vec.push_back(abs(y%10));            
-            y = y/10;
-        }
-        
-        for(int i=0,j=vec.size()-1;i<=vec.size()/2;i++,j--)
-        {
-            if(vec[i]==vec[j])
-                continue;
-            else
-                return false;
-        }
-        return true;
+        else 
+            return false;
     }
 };
