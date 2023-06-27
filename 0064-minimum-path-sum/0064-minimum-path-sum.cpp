9b5acc193bc84e907f17ed819d0dac665d1e141e
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-06-27 23:53:21 +0530

    Time: 16 ms (17.39%), Space: 10.1 MB (15.47%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,34 +1,32 @@
 class Solution 
 {
 public:
-    int paths(int a, int b, vector<vector<int>>& grid, vector<vector<int>> &dp)
-    {
-        if(a==0 && b==0)
-            return dp[a][b] = grid[a][b];
-        
-        if(a<0 || b<0)
-            return INT_MAX;         //Since we need min sum, while comparison this path will never be considered
-        
-        if(dp[a][b] != -1)
-            return dp[a][b];
-        
-        int left, up;
-        if(paths(a, b-1, grid, dp) == INT_MAX)
-            left = INT_MAX;
-        else
-            left = grid[a][b] + paths(a, b-1, grid, dp);
-        if(paths(a-1, b, grid, dp) == INT_MAX)
-            up = INT_MAX;
-        else
-            up = grid[a][b] + paths(a-1, b, grid, dp);
-        
-        return dp[a][b] = min(up, left);
-    }
+  
     int minPathSum(vector<vector<int>>& grid) 
     {
         int a = grid.size();
         int b = grid[0].size();
-        vector<vector<int>> dp(a, vector<int>(b, -1));
-        return paths(a-1, b-1, grid, dp);
+        vector<vector<int>> dp(a, vector<int>(b, -1));     
+        
+        for(int i=0;i<a;i++)
+        {
+            for(int j=0;j<b;j++)
+            {
+                if(i==0 && j==0)
+                     dp[0][0] = grid[0][0];
+                else
+                {
+                    int up = INT_MAX, left = INT_MAX;
+                    if(i>0)
+                        up = grid[i][j] + dp[i-1][j];
+                    if(j>0)
+                        left = grid[i][j] + dp[i][j-1];
+                    
+                    dp[i][j] = min(up, left);
+                }
+            }
+        }
+        
+        return dp[a-1][b-1];
     }
 };
