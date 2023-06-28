ac6a665a15251dcd93062a94dda7e2d44aa474c9
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-06-28 19:52:17 +0530

    Time: 20 ms (38.27%), Space: 13.3 MB (50.99%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -6,30 +6,44 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         
-        vector<int> rows(m, 1);
-        vector<int> cols(n, 1);
-        
+        int temp = 1;
         for(int i=0;i<m; i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(matrix[i][j] == 0)
                 {
-                    rows[i] = 0;
-                    cols[j] = 0;
+                    if(j==0)
+                    {
+                        temp = 0;
+                        matrix[i][j] = 0;
+                    }
+                    else
+                    {
+                        matrix[i][0] = 0;
+                        matrix[0][j] = 0;
+                    }
                 }
             }
         }
         
-        for(int i=0;i<m; i++)
+        for(int i = m-1; i>0 ; i--)
         {
-            for(int j=0;j<n;j++)
+            for(int j = n-1; j>0 ; j--)
             {
-                if(rows[i] == 0 || cols[j] == 0)
-                {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
                     matrix[i][j] = 0;
-                }
             }
+        }
+        for(int j = n-1; j>0; j--)
+        {
+            if(matrix[0][0] == 0 || matrix[0][j] == 0)
+                    matrix[0][j] = 0;
+        }
+        for(int i = m-1; i>=0 ; i--)
+        {
+            if(matrix[i][0] == 0 || temp == 0)
+                    matrix[i][0] = 0;
         }
     }
 };
