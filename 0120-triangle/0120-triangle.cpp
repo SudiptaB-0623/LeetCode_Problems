1e18cb2a1619d77d70e0b6a5acc793796fbd5058
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-06-29 12:24:15 +0530

    Time: 8 ms (56.29%), Space: 9 MB (22.21%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -20,6 +20,23 @@ public:
     {
         int n = triangle.size();
         vector<vector<int>> dp(n, vector<int>(n, -1));
-        return minPath(0, 0, dp, triangle);
+        
+        for(int j = n-1; j>=0; j--)
+        {
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        
+        for(int i = n-2; i>=0 ; i--)
+        {
+            for(int j = i; j>=0 ; j--)
+            {
+                int down = triangle[i][j] + dp[i+1][j];
+                int diag = triangle[i][j] + dp[i+1][j+1];
+                
+                dp[i][j] = min(down, diag);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
