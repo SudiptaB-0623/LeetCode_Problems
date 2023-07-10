63e4bc2681b5c7e6e65fdabc63859d3d9dfc40c2
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-07-10 18:34:13 +0530

    Time: 253 ms (38.12%), Space: 88.9 MB (5.02%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,30 +1,40 @@
 class Solution 
 {
 public:
-    int sumWays(int ind, int target, vector<int> nums, map<pair<int, int>, int> &dp)
+    int findways(int ind, int target, vector<int> nums, vector<vector<int>> &dp)
     {
         if(ind == 0)
         {
-            if(target+nums[0]==0 && target-nums[0]==0)
+            if(target==0 && nums[0]==0)
                 return 2;
-            else if(target+nums[0]==0 || target-nums[0]==0)
+            if(target==0 || target==nums[0])
                 return 1;
-            
             return 0;
         }
         
-        if(dp.find({ind, target}) != dp.end())
-            return dp[{ind, target}];
+        if(dp[ind][target] != -1)
+            return dp[ind][target];
         
-        int plus = sumWays(ind-1, target+nums[ind], nums, dp);
-        int minus = sumWays(ind-1, target-nums[ind], nums, dp);
+        int notTake = findways(ind-1, target, nums, dp);
+        int take = 0;
+        if(nums[ind]<=target)
+            take = findways(ind-1, target-nums[ind], nums, dp);
         
-        return dp[{ind, target}] = plus+minus;
+        return dp[ind][target] = (take+notTake);
     }
     int findTargetSumWays(vector<int>& nums, int target) 
     {
+        int totSum = 0;
         int n = nums.size();
-        map<pair<int, int>, int> dp;
-        return sumWays(n-1, target, nums, dp);
+        for(int i=0; i<n ; i++)
+            totSum += nums[i];
+        
+        if(totSum-target<0 || (totSum-target)%2!=0)
+            return 0;        
+        
+        int d = (totSum-target)/2;
+        vector<vector<int>> dp(n, vector<int>(d+1, -1));
+        cout<<d<<endl;
+        return findways(n-1, d, nums, dp);
     }
 };
