a5b1a060cc3365a0d2b79ce62a6da10ad69c0f8f
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-07-15 11:25:59 +0530

    Time: 2668 ms (7.11%), Space: 422.6 MB (9.86%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,18 +1,29 @@
 class Solution 
 {
 public:
+    int distinctSub(int ind1, int ind2, string s, string t, vector<vector<int>> &dp)
+    {
+        if(ind2<0)
+            return 1;
+        if(ind1<0)
+            return 0;
+        
+        if(dp[ind1][ind2] != -1)
+            return dp[ind1][ind2];
+        
+        if(s[ind1] == t[ind2])
+            return dp[ind1][ind2] = distinctSub(ind1-1, ind2-1, s, t, dp)+distinctSub(ind1-1, ind2, s, t, dp);
+        else
+            return dp[ind1][ind2] = distinctSub(ind1-1, ind2, s, t, dp);
+    }
     int numDistinct(string s, string t) 
     {
-        int n = s.length(), m = t.length();
-        vector<double> dyd(m+1, 0);
-        dyd[0] = 1;
-        for(int i = 1; i <= n; i++) 
-        {
-            for(int j = m; j > 0; j--) 
-            {
-                if(s[i-1] == t[j-1]) dyd[j] = dyd[j-1] + dyd[j];
-            }
-        }
-        return dyd[m];    
+        int n = s.size();
+        int m = t.size();
+        
+        if(n<m)
+            return 0;
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return distinctSub(n-1, m-1, s, t, dp);
     }
 };
