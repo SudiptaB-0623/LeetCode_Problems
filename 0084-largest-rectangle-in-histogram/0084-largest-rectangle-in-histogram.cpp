68a8ddd7e822db75e8dbe351c6613b8c63584f2f
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-07-19 15:15:26 +0530

    Time: 195 ms (75.65%), Space: 77.1 MB (79.54%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -5,51 +5,23 @@ public:
     {
         int n = heights.size();
         stack<int> st;
-        vector<int> left(n), right(n);
+        int ans = 0;
         
-        for(int i=0 ; i<n ; i++)
+        for(int i=0 ; i<=n ; i++)
         {
-            if(st.empty())
+            while(!st.empty() && (i==n || heights[i]<heights[st.top()]))
             {
-                left[i] = -1;
-            }
-            else
-            {
-                while(st.empty()!=true && heights[st.top()]>=heights[i])
-                    st.pop();
+                int right = -1, mid = -1, left = -1;                
+                right = i;
+                mid = st.top();
+                st.pop();
+                left = st.empty() ? -1 : st.top();
                 
-                if(st.empty())
-                    left[i] = -1;
-                else
-                    left[i] = st.top();
+                int curr = heights[mid] * (right-left-1);
+                ans = max(ans, curr);
             }
             st.push(i);
         }
-        st = {};
-        for(int i=n-1 ; i>=0 ; i--)
-        {
-            if(st.empty())
-            {
-                right[i] = n;
-            }
-            else
-            {
-                while(st.empty()!=true && heights[st.top()]>=heights[i])
-                    st.pop();
-                
-                if(st.empty())
-                    right[i] = n;
-                else
-                    right[i] = st.top();
-            }
-            st.push(i);
-        }
-        int maxi = 0;
-        for(int i=0; i<n ; i++)
-        {
-            int curr = heights[i]*(right[i]-left[i]-1);
-            maxi = max(maxi, curr);
-        }
-        return maxi;
+        return ans;
     }
 };
