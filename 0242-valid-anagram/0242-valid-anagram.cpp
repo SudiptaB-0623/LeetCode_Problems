dd4d737fe2dec93cf1147c99ed0876dadf284b61
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-07-22 07:12:14 +0530

    Time: 15 ms (44.93%), Space: 7.5 MB (12.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,13 +1,28 @@
-class Solution {
+class Solution 
+{
 public:
-    bool isAnagram(string s, string t) {
-        if(s.size()!=t.size()) return false;
-        int count[26]={0};
-        for(int i=0;i<s.size();i++) 
-            count[s[i]-'a']++;
-        for(int i=0;i<s.size();i++){
-            count[t[i]-'a']--;
-            if(count[t[i]-'a']<0) return false;
+    bool isAnagram(string s, string t) 
+    {
+        if(s.size() != t.size())
+            return false;
+        
+        map<int, int> mpp;
+        int n = s.size();
+        int m = t.size();
+        
+        for(int i=0 ; i<n ; i++)
+        {
+            mpp[s[i]]++;
+        }
+        for(int i=0; i<m; i++)
+        {
+            mpp[t[i]]--;
+        }
+        
+        for(auto it : mpp)
+        {
+            if(it.second != 0)
+                return false;
         }
         return true;
     }
