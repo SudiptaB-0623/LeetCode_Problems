7e55cd508dbae5af9780fb53ae787eb81e5176ec
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-08-16 23:45:03 +0530

    Time: 11 ms (72.19%), Space: 8.3 MB (29.24%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,73 +1,28 @@
 class Solution 
 {
-    public:
-        int romanToInt(string s) 
+public:
+    int romanToInt(string s) 
+    {
+        int integer = 0;
+        int n = s.size();
+        
+        map<char, int> mpp;
+        mpp['I'] = 1;
+        mpp['V'] = 5;
+        mpp['X'] = 10;
+        mpp['L'] = 50;
+        mpp['C'] = 100;
+        mpp['D'] = 500;
+        mpp['M'] = 1000;
+        
+        for(int i=0 ; i<n ; i++)
         {
-            map<char,int> mpp;
-            mpp['I']=1;
-            mpp['V']=5;
-            mpp['X']=10;
-            mpp['L']=50;
-            mpp['C']=100;
-            mpp['D']=500;
-            mpp['M']=1000;
-            
-            map<char,int> :: iterator it;
-            
-            int sum=0;
-            
-            for(int i=s.size()-1;i>=0;i--)
-            {
-                int a = 0;
-                if((s[i]=='V')||(s[i])=='X')
-                {
-                    it = mpp.find(s[i]);
-                    a = it->second;
-                    
-                    if((i-1>=0)&&(s[i-1]=='I'))
-                    {
-                        a = a-1;
-                        i--;
-                    }   
-                    sum += a;
-                    continue;
-                }
-                if((s[i]=='L')||(s[i])=='C')
-                {
-                    auto it = mpp.find(s[i]);
-                    a = it->second;
-                    
-                    if((i-1>=0)&&(s[i-1]=='X'))
-                    {
-                        a = a-10;
-                        i--;
-                    }     
-                    sum += a;
-                    continue;
-                }
-                if((s[i]=='D')||(s[i])=='M')
-                {
-                    auto it = mpp.find(s[i]);
-                    a = it->second;
-                    
-                    if((i-1>=0)&&(s[i-1]=='C'))
-                    {
-                        a = a-100;
-                        i--;
-                    }
-                    sum += a;
-                    continue;
-                }
-                if(s[i]=='I')
-                {
-                    a=1;
-                    sum += a;
-                    continue;
-                }
-                
-                
-            }
-            
-            return sum;
+            if(i+1 == n || mpp[s[i]] >= mpp[s[i+1]])
+                integer += mpp[s[i]];
+            else
+                integer -= mpp[s[i]];
         }
+        
+        return integer;
+    }
 };
