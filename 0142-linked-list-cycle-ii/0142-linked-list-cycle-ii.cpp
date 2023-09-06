951b7f7391446e37196cd49a95267311199a2790
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-09-06 21:46:17 +0530

    Time: 8 ms (65.21%), Space: 7.6 MB (87.75%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -11,33 +11,31 @@ class Solution
 public:
     ListNode *detectCycle(ListNode *head) 
     {
-        if(head==NULL || head->next==NULL)
+        if(head == NULL || head->next == NULL)
             return NULL;
         
-        ListNode *slow = head->next, *fast = (head->next)->next;
+        ListNode *slow = head;
+        ListNode *fast = head;
         
-        while(fast!=NULL && fast!=slow)
-        {            
-            slow = slow->next;
-            if(fast->next==NULL)
-                return NULL;
-            fast = (fast->next)->next;
-        }
-        
-        if(fast==NULL)
-            return NULL;
-        
-        if(fast==head)
-            return head;
-        else
+        while(fast!=NULL && fast->next!=NULL)
         {
-            slow=head;
-            while(fast!=slow)
-            {            
-                slow = slow->next;
-                fast = fast->next;
-            }
-            return fast;
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if(slow == fast)
+                break;
         }
+        
+        if(fast == NULL || fast->next==NULL)
+            return NULL;
+        
+        slow = head;
+        while(slow != fast)
+        {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        
+        return fast;
     }
 };
