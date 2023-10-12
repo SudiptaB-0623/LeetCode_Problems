c584d09a93bbf05c5eb7a464567471f9bee89aea
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-10-12 18:37:22 +0530

    Time: 7 ms (45.42%), Space: 13.6 MB (21.04%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,16 +1,15 @@
 class Solution 
 {
 public:
-    
     int orangesRotting(vector<vector<int>>& grid) 
     {
         int n = grid.size();
         int m = grid[0].size();
-        int time = 0, ans = 0;
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        int drow[] = {-1, 0, 0, 1};
-        int dcol[] = {0, -1, 1, 0};
         queue<pair<pair<int, int>, int>> q;
+        int ans = 0;
+        int drow[] = {0, -1, 1, 0};
+        int dcol[] = {-1, 0, 0, 1};
+        vector<vector<int>> vis(n, vector<int>(m, 0));
         
         for(int i=0 ; i<n ; i++)
         {
@@ -19,15 +18,14 @@ public:
                 if(grid[i][j] == 2)
                     q.push({{i, j}, 0});
             }
-        }       
+        }
         
         while(!q.empty())
         {
             int r = q.front().first.first;
             int c = q.front().first.second;
-            time = q.front().second;
+            int time = q.front().second;
             ans = max(ans, time);
-            
             q.pop();
             
             for(int i=0 ; i<4 ; i++)
@@ -35,24 +33,23 @@ public:
                 int currRow = r+drow[i];
                 int currCol = c+dcol[i];
                 
-                if(currRow>=0 && currRow<n && currCol>=0 && currCol<m && vis[currRow][currCol]==0 && grid[currRow][currCol]!=0 && grid[currRow][currCol]!=2)
+                if(currRow>=0 && currRow<n && currCol>=0 && currCol<m && vis[currRow][currCol]==0 && grid[currRow][currCol]==1)
                 {
+                    vis[currRow][currCol] = 1;
                     q.push({{currRow, currCol}, time+1});
-                    vis[currRow][currCol]=1;
-                    grid[currRow][currCol]=2;
+                    grid[currRow][currCol] = 2;
                 }
             }
         }
         
-        for(int i=0 ; i<n ; i++)
+        for(int i=0; i<n ; i++)
         {
             for(int j=0 ; j<m ; j++)
             {
-                if(grid[i][j]==1)
+                if(grid[i][j] == 1)
                     return -1;
             }
         }
-        
         return ans;
     }
 };
