49fad18264879743ff8efe1caa2aaf1217f0c30e
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2024-05-05 10:39:33 +0530

    Time: 11 ms (44.16%), Space: 7.1 MB (34.93%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -3,15 +3,11 @@ class Solution
 public:
     int fib(int n) 
     {
-        if(n==0)
+        if(n == 0)
             return 0;
-        if(n==1)
+        if(n == 1)
             return 1;
         
-        int ans = 0;
-        
-        ans = fib(n-1) + fib(n-2);
-        
-        return ans;
+        return fib(n-1) + fib(n-2);
     }
 };
