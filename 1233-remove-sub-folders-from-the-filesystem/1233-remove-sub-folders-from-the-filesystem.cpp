091eff64d794f92809c6a19f6ce2150f8366108c
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2024-10-25 10:17:52 +0530

    Time: 49 ms (94.78%), Space: 47.7 MB (75.82%) - LeetHub

---
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
@@ -0,0 +1,22 @@
+class Solution 
+{
+public:
+    vector<string> removeSubfolders(vector<string>& folder) 
+    {
+        vector<string> ans;
+        
+        sort(folder.begin(), folder.end());
+        ans.push_back(folder[0]);
+        
+        for(int i=1; i<folder.size(); i++)
+        {
+            string last = ans.back();
+            last += '/';
+            
+            if(folder[i].compare(0, last.size(), last) != 0)
+                ans.push_back(folder[i]);
+        }
+        
+        return ans;
+    }
+};
