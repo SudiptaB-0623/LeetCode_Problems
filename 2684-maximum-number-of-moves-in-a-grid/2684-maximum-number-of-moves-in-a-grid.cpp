62441331fecea3266a0b5c8b73a722c3850bf350
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2024-10-29 10:28:38 +0530

    Time: 43 ms (30.44%), Space: 74.4 MB (43.37%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -8,7 +8,7 @@ public:
 
         vector<vector<int>> dp(m, vector<int>(n, 0));
         
-        for (int i = 0; i<m; i++) 
+        for (int i=0; i<m; i++) 
             dp[i][0] = 1;
 
         int ans = 0;
@@ -16,12 +16,14 @@ public:
         {
             for (int i=0; i<m; i++) 
             {                
-                if (grid[i][j] > grid[i][j - 1] && dp[i][j - 1] > 0) 
-                    dp[i][j] = max(dp[i][j], dp[i][j - 1] + 1);
-                if (i - 1 >= 0 && grid[i][j] > grid[i - 1][j - 1] && dp[i - 1][j - 1] > 0) 
-                    dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1);
-                if (i + 1 < m && grid[i][j] > grid[i + 1][j - 1] && dp[i + 1][j - 1] > 0) 
-                    dp[i][j] = max(dp[i][j], dp[i + 1][j - 1] + 1);
+                if ((grid[i][j] > grid[i][j-1]) && (dp[i][j-1] > 0)) 
+                    dp[i][j] = max(dp[i][j], dp[i][j-1] + 1);
+                
+                if ((i-1 >= 0) && (grid[i][j] > grid[i-1][j-1]) && (dp[i-1][j-1] > 0)) 
+                    dp[i][j] = max(dp[i][j], dp[i-1][j-1] + 1);
+                
+                if ((i+1 < m) && (grid[i][j] > grid[i+1][j-1]) && (dp[i+1][j-1] > 0)) 
+                    dp[i][j] = max(dp[i][j], dp[i+1][j-1]+1);
 
                 ans = max(ans, dp[i][j]-1);
             }
