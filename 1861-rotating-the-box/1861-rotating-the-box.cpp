6ef80570c10b910328a180d97abc1277b9299fc3
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2024-11-23 21:12:22 +0530

    Time: 239 ms (13.75%), Space: 56.2 MB (85.37%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -0,0 +1,50 @@
+class Solution 
+{
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) 
+    {
+        int m = box.size();
+        int n = box[0].size();
+        vector<vector<char>> ans(n, vector<char>(m));
+
+        for(int i = 0; i < n; i++) 
+        {
+            for(int j = 0; j < m; j++) 
+            {
+                ans[i][j] = box[j][i];
+            }
+        }
+
+        for(int i = 0; i < n; i++) 
+            reverse(ans[i].begin(), ans[i].end());
+
+        for(int j = 0; j < m; j++) 
+        {
+            for(int i = n - 1; i >= 0; i--) 
+            {
+                if(ans[i][j] == '.') 
+                {  
+                    int nextRowWithStone = -1;
+
+                    for(int k = i - 1; k >= 0; k--) 
+                    {
+                        if(ans[k][j] == '*')
+                            break; 
+                        if(ans[k][j] == '#') 
+                        { 
+                            nextRowWithStone = k;
+                            break;
+                        }
+                    }
+
+                    if(nextRowWithStone != -1) 
+                    {
+                        ans[nextRowWithStone][j] = '.';
+                        ans[i][j] = '#';
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
