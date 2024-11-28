1090d62b3f4f73ad84aeb165baf959485825cae4
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2024-11-29 00:55:47 +0530

    Time: 1040 ms (18.52%), Space: 234 MB (20.83%) - LeetHub

---
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -0,0 +1,52 @@
+class Solution 
+{
+public:
+    vector<vector<int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    int minimumObstacles(vector<vector<int>>& grid) 
+    {
+        int n = grid[0].size();
+        int m = grid.size();        
+
+        vector<vector<int>> minObstacles(m, vector<int>(n, INT_MAX));
+        minObstacles[0][0] = grid[0][0];
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+
+        pq.push({minObstacles[0][0], 0, 0});
+
+        while(!pq.empty())
+        {
+            vector<int> current = pq.top();
+            pq.pop();
+            int obstacles = current[0];
+            int row = current[1];
+            int col = current[2];
+
+            if(row == m - 1 && col == n - 1) 
+                return obstacles;
+
+            for(vector<int>& dir : directions) 
+            {
+                int newRow = row + dir[0], newCol = col + dir[1];
+
+                if (isValid(grid, newRow, newCol)) 
+                {
+                    int newObstacles = obstacles + grid[newRow][newCol];
+
+                    if (newObstacles < minObstacles[newRow][newCol]) 
+                    {
+                        minObstacles[newRow][newCol] = newObstacles;
+                        pq.push({newObstacles, newRow, newCol});
+                    }
+                }
+            }
+        }
+
+        return minObstacles[m - 1][n - 1];
+    }
+
+    bool isValid(vector<vector<int>>& grid, int row, int col) 
+    {
+        return row >= 0 && col >= 0 && row < grid.size() && col < grid[0].size();
+    }
+};
