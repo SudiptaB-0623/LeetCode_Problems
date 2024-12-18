885433d56cb4b1af5cbe264008765ead5f996eac
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2024-12-19 02:49:19 +0530

    Time: 0 ms (100.00%), Space: 14 MB (25.89%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,23 @@
+class Solution 
+{
+public:
+    vector<int> finalPrices(vector<int>& prices) 
+    {
+        int n = prices.size();
+        vector<int> result = prices;
+
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (prices[j] <= prices[i])
+                {
+                    result[i] = prices[i] - prices[j];
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+};
